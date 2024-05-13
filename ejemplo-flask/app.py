5c105b52ecb92c1c6ee3ab2d02a117d534be4b9e
uuunin <rdcarrion3@utpl.edu.ec>
2024-05-13 12:49:04 -0500

    Práctica finalizada, el link del vídeo se encuentra en el archivo link-video.txt

--- ejemplo-flask/app.py
+++ ejemplo-flask/app.py
@@ -8,7 +8,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/mi/persona/<id>/mis/datos/personales/2023/loja/ecuador')
+@app.route('/persona/<id>')
 def persona(id):
     return render_template('persona.html', id=id)
 
